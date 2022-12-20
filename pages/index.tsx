5efbede538c4f6176a70714015b10e70cf07d3f9
Ionuț Alexandru Coșuleanu <ionut.alex.cosuleanu@gmail.com>
2022-12-20 02:20:37 +0100

    add tailwind and move to it (#4)

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.heading}>La <span className={styles.h}>h</span><span className={styles.o}>o</span><span className={styles.r}>r</span><span className={styles.a}>a</span><br />d<span className={styles.el}>el</span> V<span className={styles.e}>e</span><span className={styles.r}>r</span><span className={styles.m}>m</span>ú</h1>
-        <div className={styles.typewriter}>
-            <h2 className={styles.subHeading}>El chispazo de la vida.</h2>
+      <main className={`${styles.main} animate-gradient bg-[length:350%_350%] leading-normal font-['Chulapa'] font-variant-ligatures-discretionary`}>
+        <h1 className={`${styles.heading} text-8xl text-center font-bold uppercase`}>La <span className={styles.h}>h</span><span className={styles.o}>o</span><span className={styles.r}>r</span><span className={styles.a}>a</span><br />d<span className={styles.el}>el</span> V<span className={styles.e}>e</span><span className={styles.r}>r</span><span className={styles.m}>m</span>ú</h1>
+        <div className={`mt-5`}>
+            <h2 className={`overflow-hidden font-bold text-2xl border-white border-r-2 whitespace-nowrap pr-1 animate-typing uppercase`}>El chispazo de la vida.</h2>
         </div>
       </main>
     </>
