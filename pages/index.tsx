6700c13b7a8329f46b69e9d9e2b00ce95cec132c
Ionuț Alexandru Coșuleanu <ionut.alex.cosuleanu@gmail.com>
2022-12-26 16:48:49 +0100

    Audio player villancico (#5)
    * villancico player wip
    
    * delete package-lock file
    
    * audio player

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
+import AudioPlayer from '../components/AudioPlayer'
 
 export default function Home() {
   return (
@@ -10,10 +11,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={`${styles.main} animate-gradient bg-[length:350%_350%] leading-normal font-['Chulapa'] font-variant-ligatures-discretionary`}>
-        <h1 className={`${styles.heading} text-8xl text-center font-bold uppercase`}>La <span className={styles.h}>h</span><span className={styles.o}>o</span><span className={styles.r}>r</span><span className={styles.a}>a</span><br />d<span className={styles.el}>el</span> V<span className={styles.e}>e</span><span className={styles.r}>r</span><span className={styles.m}>m</span>ú</h1>
-        <div className={`mt-5`}>
-            <h2 className={`overflow-hidden font-bold text-2xl border-white border-r-2 whitespace-nowrap pr-1 animate-typing uppercase`}>El chispazo de la vida.</h2>
+      <main className={`${styles.main} animate-gradient bg-[length:350%_350%]`}>
+        <div className={`container mx-auto px-6 sm:px-2 lg:px-0`}>
+          <div className={`flex flex-col justify-center items-center leading-normal font-['Chulapa'] font-variant-ligatures-discretionary`}>
+            <h1 className={`${styles.heading} text-8xl text-center font-bold uppercase`}>La <span className={styles.h}>h</span><span className={styles.o}>o</span><span className={styles.r}>r</span><span className={styles.a}>a</span><br />d<span className={styles.el}>el</span> V<span className={styles.e}>e</span><span className={styles.r}>r</span><span className={styles.m}>m</span>ú</h1>
+            <div className={`mt-5`}>
+                <h2 className={`overflow-hidden font-bold text-2xl border-white border-r-2 whitespace-nowrap pr-1 animate-typing uppercase`}>El chispazo de la vida.</h2>
+            </div>
+          </div>
+          <div className="my-10 w-full flex justify-center">
+            <div className={`w-full sm:w-3/4 md:w-1/2 lg:w-1/3`}>
+              <AudioPlayer />
+            </div>
+          </div>
         </div>
       </main>
     </>
