0b10df3057ce2630d2dc8ace8906fae24ddb40fa
Ionut Alexandru Cosuleanu <ionut.alex.cosuleanu@gmail.com>
2022-12-28 04:42:28 +0100

    add progress bar in audio player

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -10,10 +10,44 @@ export default function AudioPlayer({
 }) {
     const [isPlaying, setIsPlaying] = useState<boolean>(false)
     const audioRef = useRef<HTMLAudioElement | null>(null)
-    const [userHasInteracted, setUserHasInteracted] = useState<boolean>(false)
+    const [progress, setProgress] = useState<number>(0)
+    const intervalRef = useRef<NodeJS.Timer | undefined>()
+
+    const duration = audioRef.current?.duration || 0
+    const progressPercentage = duration ? progress / duration * 100 : 0
+
+    const onPlayPause = () => {
+        setIsPlaying(playing => !playing)
+    }
+
+    const startTimer = () => {
+        // Clear any timers already running
+        clearInterval(intervalRef.current);
+    
+        intervalRef.current = setInterval(() => {
+            setProgress(audioRef.current?.currentTime || 0)
+        }, 1000)
+    }
+
+    const onScrub = (value : string) => {
+        if (!audioRef.current) return
+        const val = Number(value)
+        clearInterval(intervalRef.current)
+        audioRef.current.currentTime = val
+        setProgress(val)
+    }
+
+    const onScrubEnd = () => {
+        if (!isPlaying) {
+            setIsPlaying(true)
+        }
+        startTimer()
+    }
 
     useEffect(() => {
         audioRef.current = new Audio(audioSrc)
+        audioRef.current.load()
+        startTimer()
     }, [])
     
     useEffect(() => {
@@ -25,7 +59,7 @@ export default function AudioPlayer({
     }, [audioRef.current])
 
     useEffect(() => {
-        if (!audioRef.current || !userHasInteracted) {
+        if (!audioRef.current) {
             return
         }
         if (isPlaying) {
@@ -34,7 +68,7 @@ export default function AudioPlayer({
             audioRef.current.pause()
         }
 
-    }, [userHasInteracted, isPlaying, audioRef.current])
+    }, [isPlaying, audioRef.current])
 
     return (
         <div className={`w-100 backdrop-blur-sm bg-white/30 rounded`}>
@@ -44,16 +78,27 @@ export default function AudioPlayer({
                 </div>
                 <div className={`text-sm`}>{author}</div>
                 <div className={`text-xl font-bold`}>{title}</div>
-                <div className={`my-4`}>
-                    {isPlaying
-                        ? <Pause onClick={() => setIsPlaying(false)} />
-                        : <Play onClick={() => {
-                            if (!userHasInteracted) {
-                                setUserHasInteracted(true)
-                            }
-                            setIsPlaying(true)
-                        }} />
-                    }
+                <div className={`my-4 w-full flex items-center`}>
+                    <>
+                        {isPlaying
+                            ? <Pause onClick={onPlayPause} className={`mr-2`} iconClassName={`h-8 w-8`} />
+                            : <Play onClick={onPlayPause} className={`mr-2`} iconClassName={`h-8 w-8`} />
+                        }
+                    </>
+                    <input
+                        type="range"
+                        min={0}
+                        max={audioRef.current?.duration}
+                        step={1}
+                        value={progress}
+                        onChange={(e) => onScrub(e.target.value)}
+                        onMouseUp={onScrubEnd}
+                        onKeyUp={onScrubEnd}
+                        className="audio-player-progress grow h-2 appearance-none cursor-pointer bg-gradient-to-r from-white to-gray-400"
+                        style={{
+                            '--tw-gradient-stops': `var(--tw-gradient-from) 0% ${progressPercentage}%, var(--tw-gradient-to) ${progressPercentage}% 100%`
+                        }}
+                    />
                 </div>
             </div>
         </div>
