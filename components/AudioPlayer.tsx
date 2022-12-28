8e094d38e4a4ebc81a667872d65f043f13d2607d
Ionuț Alexandru Coșuleanu <ionut.alex.cosuleanu@gmail.com>
2022-12-28 05:01:53 +0100

    add progress bar in audio player (#6)
    * add progress bar in audio player
    
    * fix hook dependencies

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -1,7 +1,16 @@
 import Play from "./Play"
 import Pause from "./Pause"
-import { useState, useRef, useEffect } from "react"
+import { useState, useRef, useEffect, useMemo, CSSProperties } from "react"
+import Image from 'next/image'
 
+const startTimer = (intervalRef : any, audioRef : any, setProgress : any) => {
+    // Clear any timers already running
+    clearInterval(intervalRef.current);
+
+    intervalRef.current = setInterval(() => {
+        setProgress(audioRef.current?.currentTime || 0)
+    }, 1000)
+}
 
 export default function AudioPlayer({
     author='La hora del vermú',
@@ -10,11 +19,37 @@ export default function AudioPlayer({
 }) {
     const [isPlaying, setIsPlaying] = useState<boolean>(false)
     const audioRef = useRef<HTMLAudioElement | null>(null)
-    const [userHasInteracted, setUserHasInteracted] = useState<boolean>(false)
+    const [progress, setProgress] = useState<number>(0)
+    const intervalRef = useRef<NodeJS.Timer | undefined>()
+
+    const duration = audioRef.current?.duration || 0
+    const progressPercentage = duration ? progress / duration * 100 : 0
+
+    const onPlayPause = () => {
+        setIsPlaying(playing => !playing)
+    }
+
+
+    const onScrub = (value : string) => {
+        if (!audioRef.current) return
+        const val = Number(value)
+        clearInterval(intervalRef.current)
+        audioRef.current.currentTime = val
+        setProgress(val)
+    }
+
+    const onScrubEnd = () => {
+        if (!isPlaying) {
+            setIsPlaying(true)
+        }
+        startTimer(intervalRef, audioRef, setProgress)
+    }
 
     useEffect(() => {
         audioRef.current = new Audio(audioSrc)
-    }, [])
+        audioRef.current.load()
+        startTimer(intervalRef, audioRef, setProgress)
+    }, [audioSrc])
     
     useEffect(() => {
         if (!audioRef.current) return
@@ -22,10 +57,10 @@ export default function AudioPlayer({
         return (() => {
             audioRef.current?.removeEventListener('ended', () => setIsPlaying(false))
         })
-    }, [audioRef.current])
+    }, [])
 
     useEffect(() => {
-        if (!audioRef.current || !userHasInteracted) {
+        if (!audioRef.current) {
             return
         }
         if (isPlaying) {
@@ -34,26 +69,37 @@ export default function AudioPlayer({
             audioRef.current.pause()
         }
 
-    }, [userHasInteracted, isPlaying, audioRef.current])
+    }, [isPlaying])
 
     return (
         <div className={`w-100 backdrop-blur-sm bg-white/30 rounded`}>
             <div className={`w-100 p-3 flex flex-col justify-content-center items-center gap-2`}>
                 <div className={`w-48 h-48 rounded-full overflow-hidden`}>
-                    <img className="w-48 h-48 transition-all duration-500 object-cover grayscale hover:grayscale-0 hover:scale-110" src="/img/AudioCover.jpeg" />
+                    <Image className="w-48 h-48 transition-all duration-500 object-cover grayscale hover:grayscale-0 hover:scale-110" src="/img/AudioCover.jpeg" alt="Audio Cover" width="240" height="240" />
                 </div>
                 <div className={`text-sm`}>{author}</div>
                 <div className={`text-xl font-bold`}>{title}</div>
-                <div className={`my-4`}>
-                    {isPlaying
-                        ? <Pause onClick={() => setIsPlaying(false)} />
-                        : <Play onClick={() => {
-                            if (!userHasInteracted) {
-                                setUserHasInteracted(true)
-                            }
-                            setIsPlaying(true)
-                        }} />
-                    }
+                <div className={`my-4 w-full flex items-center`}>
+                    <>
+                        {isPlaying
+                            ? <Pause onClick={onPlayPause} className={`mr-2`} iconClassName={`h-8 w-8`} />
+                            : <Play onClick={onPlayPause} className={`mr-2`} iconClassName={`h-8 w-8`} />
+                        }
+                    </>
+                    <input
+                        type="range"
+                        min={0}
+                        max={audioRef.current?.duration}
+                        step={1}
+                        value={progress}
+                        onChange={(e) => onScrub(e.target.value)}
+                        onMouseUp={onScrubEnd}
+                        onKeyUp={onScrubEnd}
+                        className="audio-player-progress grow h-2 appearance-none cursor-pointer bg-gradient-to-r from-white to-gray-400"
+                        style={{
+                            '--tw-gradient-stops': `var(--tw-gradient-from) 0% ${progressPercentage}%, var(--tw-gradient-to) ${progressPercentage}% 100%`
+                        } as CSSProperties}
+                    />
                 </div>
             </div>
         </div>
--- components/Pause.tsx
+++ components/Pause.tsx
@@ -1,10 +1,11 @@
 export default function Pause({
     onClick = () => console.log('Pausing'),
     className='',
+    iconClassName='h-6 w-6',
 }) {
     return (
         <button className={className} onClick={onClick}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={iconClassName}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25v13.5m-7.5-13.5v13.5" />
             </svg>
         </button>
