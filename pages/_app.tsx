d6657a1f0ea189f8b327a4ab7b7d429fe9c0354e
Ionuț Alexandru Coșuleanu <ionut.alex.cosuleanu@gmail.com>
2023-02-06 18:37:24 +0100

    remove villancico

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script src="../js/snow.js" defer></Script>
       <Component {...pageProps} />
     </>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
-import AudioPlayer from '../components/AudioPlayer'
 
 export default function Home() {
   return (
@@ -17,11 +16,6 @@ export default function Home() {
             <h1 className={`${styles.heading} text-8xl text-center font-bold uppercase`}>La <span className={styles.h}>h</span><span className={styles.o}>o</span><span className={styles.r}>r</span><span className={styles.a}>a</span><br />d<span className={styles.el}>el</span> V<span className={styles.e}>e</span><span className={styles.r}>r</span><span className={styles.m}>m</span>ú</h1>
             <div className={`mt-5`}>
                 <h2 className={`overflow-hidden font-bold text-2xl border-white border-r-2 whitespace-nowrap pr-1 animate-typing uppercase`}>El chispazo de la vida.</h2>
-            </div>
-          </div>
-          <div className="my-10 w-full flex justify-center">
-            <div className={`w-full sm:w-3/4 md:w-1/2 lg:w-1/3`}>
-              <AudioPlayer />
             </div>
           </div>
         </div>
